8b95adaa76210cd1367db2c17e7b1e037edd0544
Balakrishna Soura <soura_balakrishna49@yahoo.com>
2021-06-26 21:35:31 -0700

    Update updatingcsv.py

--- updatingcsv.py
+++ updatingcsv.py
@@ -1,9 +1,16 @@
 import csv
 from tempfile import NamedTemporaryFile
 import shutil
+import datetime
 
 
 
+def updated_email():
+    email_prefix = "test"
+    email_suffix = "@mailinator.com"
+    today = datetime.datetime.now()
+    date_time = today.strftime("%m%d%Y%H%M%S")
+    return email_prefix+date_time+email_suffix
 
 
 def update_new_user():
@@ -15,10 +22,13 @@ def update_new_user():
         csv_writer = csv.writer(tempfile)
         for line in csv_reader:
             if line[0] != 'First Name':
-                line[3] = 'balathegreat'+ line[0]
-                print[line[3]]
+                line[3] = updated_email()
+                print(line[3])
             csv_writer.writerow(line)
 
     shutil.move(tempfile.name,filename)
 
-    
+
+
+
+update_new_user()
