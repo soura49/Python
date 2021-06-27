4d334afa2cb9bf2bdbe7418a16ff86787d8cfb22
Balakrishna Soura <soura_balakrishna49@yahoo.com>
2021-06-26 21:14:29 -0700

    Update updatingcsv.py

--- updatingcsv.py
+++ updatingcsv.py
@@ -2,18 +2,21 @@ import csv
 from tempfile import NamedTemporaryFile
 import shutil
 
-filename = '/Users/balakrishnasoura/Desktop/boto3/exported_users.csv'
-
-tempfile = NamedTemporaryFile(mode='w',delete=False)
 
 
-with open(filename,'r') as csv_file,tempfile:
-    csv_reader = csv.reader(csv_file)
-    csv_writer = csv.writer(tempfile)
-    for line in csv_reader:
-        if line[0] != 'First Name':
-            line[3] = 'balathegreat'+ line[0]
-            print[line[3]]
-        csv_writer.writerow(line)
 
-shutil.move(tempfile.name,filename)
+
+def update_new_user():
+    filename = '/Users/balakrishnasoura/Desktop/boto3/exported_users.csv'
+
+    tempfile = NamedTemporaryFile(mode='w',delete=False)
+    with open(filename,'r') as csv_file,tempfile:
+        csv_reader = csv.reader(csv_file)
+        csv_writer = csv.writer(tempfile)
+        for line in csv_reader:
+            if line[0] != 'First Name':
+                line[3] = 'balathegreat'+ line[0]
+                print[line[3]]
+            csv_writer.writerow(line)
+
+    shutil.move(tempfile.name,filename)
